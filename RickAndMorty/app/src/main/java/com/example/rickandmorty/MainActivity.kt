84fdceb4f0324fed7a9cb1c31caa2e1f324f835c
Luis Andia <luis.andia@amigocloud.com>
2021-11-23 11:24:10 -0500

    Adding picasso and view to show the data

--- RickAndMorty/app/src/main/java/com/example/rickandmorty/MainActivity.kt
+++ RickAndMorty/app/src/main/java/com/example/rickandmorty/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.rickandmorty
 
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -9,7 +10,20 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.appcompat.widget.AppCompatTextView
 import com.example.rickandmorty.databinding.ActivityMainBinding
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import com.squareup.picasso.Picasso
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,16 +36,59 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setSupportActionBar(binding.toolbar)
+        val nameTextView = findViewById<AppCompatTextView>(R.id.nameTextView)
+        val headerImageView = findViewById<AppCompatImageView>(R.id.headerImageView)
+        val genderImageView = findViewById<AppCompatImageView>(R.id.genderImageView)
+        val aliveTextView = findViewById<AppCompatTextView>(R.id.aliveTextView)
+        val originTextView = findViewById<AppCompatTextView>(R.id.originTextView)
+        val speciesTextView = findViewById<AppCompatTextView>(R.id.speciesTextView)
 
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
+        val moshi = Moshi.Builder()
+            .addLast(KotlinJsonAdapterFactory())
+            .build()
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://rickandmortyapi.com/api/")
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+        val rickAndMortyService: RickAndMortyService = retrofit.create(RickAndMortyService::class.java)
 
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
+        rickAndMortyService.getCharacterById(10).enqueue(object : Callback<GetCharacterByIdResponse> {
+            override fun onResponse(call: Call<GetCharacterByIdResponse>, response: Response<GetCharacterByIdResponse>) {
+                Log.i("MainActivity", response.toString())
+
+                if (!response.isSuccessful) {
+                    Toast.makeText(
+                        this@MainActivity,
+                        "Unsuccessful network call!",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    return
+                }
+
+                val body = response.body()!!
+
+                nameTextView.text = body.name
+                aliveTextView.text = body.status
+                speciesTextView.text = body.species
+                originTextView.text = body.origin.name
+
+                genderImageView.setImageResource(R.drawable.ic_female_24)
+
+                if(body.gender.equals("male",true)){
+                    genderImageView.setImageResource(R.drawable.ic_male_24)
+                }
+
+                Picasso.get().load(body.image).into(headerImageView)
+
+            }
+
+            override fun onFailure(call: Call<GetCharacterByIdResponse>, t: Throwable) {
+                Log.i("MainActivity",t.message ?:"null message")
+            }
+
+        })
+
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
